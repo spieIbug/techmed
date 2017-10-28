da427e5a4dafb5f5c735b34772e4e972d930a056
ymeddah <my.meddah@gmail.com>
2017-10-28 22:22:03 +0200

    UI labels instead of ids + price binding

--- src/main/webapp/app/entities/tarif/tarif.model.ts
+++ src/main/webapp/app/entities/tarif/tarif.model.ts
@@ -1,16 +1,45 @@
-import { BaseEntity } from './../../shared';
+import {BaseEntity} from './../../shared';
 
 export class Tarif implements BaseEntity {
-    constructor(
-        public id?: number,
-        public prixHT?: number,
-        public tva?: number,
-        public prixTTC?: number,
-        public actif?: boolean,
-        public dateDebut?: any,
-        public dateFin?: any,
-        public acteMedicalId?: number,
-    ) {
+    constructor(public id?: number,
+                private _prixHT?: number,
+                private _tva?: number,
+                private _prixTTC?: number,
+                public actif?: boolean,
+                public dateDebut?: any,
+                public dateFin?: any,
+                public acteMedicalId?: number) {
+
+        _prixHT = 0;
+        _tva = 0;
+        _prixTTC = 0;
         this.actif = false;
+    }
+
+    public get tva(): number {
+        return this._tva;
+    }
+
+    public set tva(value: number) {
+        this._tva = value;
+        this._prixTTC = this._prixHT * (100 + this._tva) / 100;
+    }
+
+    public set prixHT(value: number) {
+        this._prixHT = value;
+        this._prixTTC = this._prixHT * (100 + this._tva) / 100;
+    }
+
+    public get prixHT(): number {
+        return this._prixHT;
+    }
+
+    public get prixTTC(): number {
+        return this._prixTTC;
+    }
+
+    public set prixTTC(value: number) {
+        this._prixTTC = value;
+        this._prixHT = this._prixTTC * 100 / (100 + this._tva);
     }
 }
